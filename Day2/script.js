83e8d8a9e342613d9234cf8a3808db1742c4c1bb
MuhammadDan <daniyalk154330@gmail.com>
2025-08-19 00:11:14 +0500

    Day2 complete

---
+++ Day2/script.js
@@ -0,0 +1,12 @@
+// Create your product object
+export const product = {
+    id: 1,
+    name: "Ali",
+    price: 45000,
+    availability: true,
+}
+// export your product object
+
+// console.log the object to view the output
+console.log(product)
+// TIP: Use the run button to run your JS Code
